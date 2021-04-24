e5d7916f1b9bb8820621cfe9d478b316d6683e80
A3211 <a.kusl1122@gmail.com>
2021-04-24 23:16:52 +0700

    added owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import config.DriverConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,6 +13,9 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentHelper.*;
 
 public class TestBase {
+
+    static DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
+
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
@@ -23,10 +28,22 @@ public class TestBase {
         //gradle clean test -Dweb.browser=opera
         Configuration.browser = (System.getProperty("web.browser", "chrome"));
 
+        String user = driverConfig.remoteWebUser();
+        String password = driverConfig.remoteWebPassword();
         String remoteWebDriver = System.getProperty("remote.web.driver");
         if (remoteWebDriver != "") {
-            Configuration.remote = remoteWebDriver;
+            Configuration.remote = String.format(remoteWebDriver, user, password);
+
+            System.out.println(String.format(remoteWebDriver, user, password));
         }
+
+        /*
+        "https://%s:%s@selenoid.autotests.cloud/wd/hub
+        String.format("%s:%s@selenoid.autotests.cloud/wd/hub", "user1", "1234")
+        -> https://user1:1234@selenoid.autotests.cloud/wd/hub
+
+        gradle clean test -Dremote.web.driver="https://%s:%s@selenoid.autotests.cloud/wd/hub"
+         */
 
         //Запускаем тесты локально
         //gradle clean test
